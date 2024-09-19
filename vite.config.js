0b64fac3d65a8b8bd60fc3a2b71c054ce5a08d03
rocio_ld <rocio_ld@hotmail.com>
2024-09-19 11:14:08 +0200

    ultimo cambio

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/https://github.com/Adalab/modulo-3-evaluacion-final-rocio-ld.git",
+  base:"/modulo-3-evaluacion-final-rocio-ld/",
 });
